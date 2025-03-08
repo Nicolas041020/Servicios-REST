664abed16919a705cebe04f7f64794491d68ae4c
Nicolás Moreno Ramírez <nm341510@gmail.com>
2025-03-07 20:02:34 -0500

    first commit

---
+++ src/main/java/com/sample/testws/entidad/Person.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sample.testws.entidad;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+/**
+ *
+ * @author nicoc
+ */
+@XmlRootElement(name = "persona")
+@XmlType(propOrder = {"id","fullName","age"})
+public class Person {
+    
+    private int id;
+    private String fullName;
+    private int age;
+    private int salary;
+    
+    public Person(){}
+
+    public Person(int id, String fullName, int age) {
+        this.id = id;
+        this.fullName = fullName;
+        this.age = age;
+        this.salary = age*1600000/3;
+    }
+
+    @XmlElement
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    @XmlElement
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+    @XmlElement
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    @XmlElement
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int age) {
+        this.salary = age*1600000/3;
+    }
+    
+    
+    
+}
---
+++ src/main/java/com/sample/testws/entidad/SalaryObj.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sample.testws.entidad;
+
+/**
+ *
+ * @author nicoc
+ */
+public class SalaryObj {
+    
+    private int salary;
+
+    public SalaryObj(int salary) {
+        this.salary = salary;
+    }
+
+    public SalaryObj(){}
+    
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+    
+    
+}
